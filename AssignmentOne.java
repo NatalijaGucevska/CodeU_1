e633508befb64c666d61ac95201fd0e46b488033
NatalijaGucevska <natalija.gucevska@epfl.ch>
2017-05-19 13:06:45 +0200

    Test cases for the fist question

---
+++ AssignmentOne.java
@@ -0,0 +1,15 @@
+
+public class AssignmentOne {
+
+	public static void main(String[] args) {
+		Q1 q1 = new Q1();
+		
+		System.out.println("Expected false, returned " + q1.isPermutation(null, null));
+		System.out.println("Expected false, returned " + q1.isPermutation("", null));
+		System.out.println("Expected false, returned " + q1.isPermutation(null, "HelLo"));
+		System.out.println("Expected false, returned " + q1.isPermutation("Hello", "Helo"));
+		System.out.println("Expected true, returned " + q1.isPermutation("Hello", "HelLo"));
+        System.out.println("Expected true, returned " + q1.isPermutation("", ""));
+	}
+
+}
