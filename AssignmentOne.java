2eea34f5d4423ef4bfafde2932e50cc6e630c277
NatalijaGucevska <natalija.gucevska@epfl.ch>
2017-05-19 23:50:54 +0200

    Fix indentation

--- AssignmentOne.java
+++ AssignmentOne.java
@@ -11,7 +11,7 @@ public class AssignmentOne {
 			System.out.println(e.getMessage());
 		}
 		
-	    // The final linked list is 1->2->3->4->5
+		// The final linked list is 1->2->3->4->5
 		list.add(1);
 		list.add(2);
 		list.add(3);
@@ -19,7 +19,7 @@ public class AssignmentOne {
 		list.add(5);
 		
 		System.out.println("Expected 4, returned " + list.kToLast(1));
-        // k > size of the list, the value of the head should be printed		
+        	// k > size of the list, the value of the head should be printed		
 		System.out.println("Expected 1, returned " + list.kToLast(6));
 	
 		Q1 q1 = new Q1();
@@ -29,9 +29,9 @@ public class AssignmentOne {
 		System.out.println("Expected false, returned " + q1.isPermutation(null, "HelLo"));
 		System.out.println("Expected false, returned " + q1.isPermutation("Hello", "Helo"));
 		System.out.println("Expected true, returned " + q1.isPermutation("Hello", "HelLo"));
-    System.out.println("Expected false, returned " + q1.isPermutation("Hi", "hellorr"));
-    System.out.println("Expected true, returned " + q1.isPermutation("", ""));
-    System.out.println("Expected true, returned " + q1.isPermutation("Hello", "elloh"));
+    		System.out.println("Expected false, returned " + q1.isPermutation("Hi", "hellorr"));
+    		System.out.println("Expected true, returned " + q1.isPermutation("", ""));
+    		System.out.println("Expected true, returned " + q1.isPermutation("Hello", "elloh"));
 
 	}
 
